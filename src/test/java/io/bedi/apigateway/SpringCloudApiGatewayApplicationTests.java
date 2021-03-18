c7bd2ac1a6e7d5efe0fc487f6ed0fec4e8d3b94e
softwarece1 <softwarece1@bel.co.in>
2021-03-18 16:20:56 +0530

    first commit

---
+++ src/test/java/io/bedi/apigateway/SpringCloudApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bedi.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
